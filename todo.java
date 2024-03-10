8477eaaff9261e732f91c31d7c7a61f8a756fe86
Sachin1785 <sachhidanandamahapatro@gmail.com>
2024-03-10 16:35:35 +0530

    adopted hasmpas and improved completion status

--- todo.java
+++ todo.java
@@ -3,18 +3,14 @@ import java.util.*;
 public class todo {
     // ANSI color codes
     public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_BLUE = "\u001B[34m";
     public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
+    public static final String ANSI_RED = "\u001B[31m";
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> tasks = new ArrayList<>();
+        Map<String, Boolean> tasks = new HashMap<>();
         System.out.print("\033[H\033[2J");
         System.out.flush();
 
@@ -27,21 +23,24 @@ public class todo {
                 case "/add": {
                     System.out.println(ANSI_BLUE + "Enter task to be added" + ANSI_RESET);
                     String task = sc.nextLine();
-                    tasks.add(task);
-                    System.out.println(ANSI_GREEN + "Task added" + ANSI_RESET);
+                    if (!tasks.containsKey(task)) {
+                        tasks.put(task, false);
+                        System.out.println(ANSI_GREEN + "Task added" + ANSI_RESET);
+                    } else {
+                        System.out.println(ANSI_RED + "Task already exists" + ANSI_RESET);
+                    }
                 }
                 break;
 
                 case "/delete": {
-                    System.out.println(ANSI_RED + "Enter task index to delete" + ANSI_RESET);
+                    System.out.println(ANSI_RED + "Enter task to delete" + ANSI_RESET);
                     displayTasksWithIndex(tasks);
-                    int index = sc.nextInt();
-                    sc.nextLine();
-                    if (index > 0 && index <= tasks.size()) {
-                        tasks.remove(index - 1);
+                    String taskToDelete = sc.nextLine();
+                    if (tasks.containsKey(taskToDelete)) {
+                        tasks.remove(taskToDelete);
                         System.out.println(ANSI_RED + "Task deleted" + ANSI_RESET);
                     } else {
-                        System.out.println(ANSI_RED + "Invalid task index" + ANSI_RESET);
+                        System.out.println(ANSI_RED + "Task not found" + ANSI_RESET);
                     }
                 }
                 break;
@@ -50,15 +49,14 @@ public class todo {
                 }
                 break;
                 case "/done": {
-                    System.out.println(ANSI_BLUE + "Enter task index to mark as done" + ANSI_RESET);
+                    System.out.println(ANSI_BLUE + "Enter task to mark as done" + ANSI_RESET);
                     displayTasksWithIndex(tasks);
-                    int index = sc.nextInt();
-                    sc.nextLine();
-                    if (index > 0 && index <= tasks.size()) {
-                        tasks.set(index - 1, tasks.get(index - 1) + "-done");
+                    String taskToMarkDone = sc.nextLine();
+                    if (tasks.containsKey(taskToMarkDone)) {
+                        tasks.put(taskToMarkDone, true);
                         System.out.println(ANSI_GREEN + "Task marked as done" + ANSI_RESET);
                     } else {
-                        System.out.println(ANSI_RED + "Invalid task index" + ANSI_RESET);
+                        System.out.println(ANSI_RED + "Task not found" + ANSI_RESET);
                     }
                 }
                 break;
@@ -83,15 +81,19 @@ public class todo {
         sc.close();
     }
 
-    // display tasks with their indices
-    public static void displayTasksWithIndex(ArrayList<String> tasks) {
+    // Method to display tasks with their indices and completion status
+    public static void displayTasksWithIndex(Map<String, Boolean> tasks) {
         if (tasks.isEmpty()) {
             System.out.println(ANSI_YELLOW + "No tasks" + ANSI_RESET);
-
         } else {
-            System.out.println(ANSI_CYAN + "Tasks:" + ANSI_RESET);
-            for (int i = 0; i < tasks.size(); i++) {
-                System.out.println((i + 1) + ". " +ANSI_PURPLE+ tasks.get(i)+ ANSI_RESET);
+            System.out.println(ANSI_YELLOW + "Tasks:" + ANSI_RESET);
+            int index = 1;
+            for (Map.Entry<String, Boolean> entry : tasks.entrySet()) {
+                String task = entry.getKey();
+                boolean isDone = entry.getValue();
+                String status = isDone ? ANSI_GREEN + "Done" + ANSI_RESET : ANSI_RED + "Not Done" + ANSI_RESET;
+                System.out.println(index + ". " + task + " - " + status);
+                index++;
             }
         }
     }
@@ -107,4 +109,3 @@ public class todo {
         System.out.println(ANSI_YELLOW + "Enter /help to display this help" + ANSI_RESET);
     }
 }
-
